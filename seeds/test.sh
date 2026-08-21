ad9f4b22e0df85c099ef588a6558eb32c2392a16
parham-k <pkazemi3@gmail.com>
2026-08-21 12:47:03 -0700

    Remove old seed generation script

--- seeds/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-diff -s <(echo "4362857412768957556
-3572411708064410444
-2319985823310095140
-2978368046464386134" | python make_internal_hpp.py | clang-format --style=file) known.hpp
